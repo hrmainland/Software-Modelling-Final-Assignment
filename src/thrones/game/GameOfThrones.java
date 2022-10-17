649923cd797765416e653b5684d4fcd12791420f
scottmadden <smmadden@student.unimelb.edu.au>
2022-10-17 20:07:07 +1100

    change seed

--- src/thrones/game/GameOfThrones.java
+++ src/thrones/game/GameOfThrones.java
@@ -494,7 +494,7 @@ public class GameOfThrones extends CardGame {
 			  seed = new Random().nextInt(); // so randomise
         }
         */
-        GameOfThrones.seed = 130006;
+        GameOfThrones.seed = 130007;
         System.out.println("Seed = " + seed);
         GameOfThrones.random = new Random(seed);
         new GameOfThrones();
